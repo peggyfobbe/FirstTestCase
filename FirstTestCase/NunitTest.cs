2208661fb67cffffd53ae1f1a0ee34f999318a7e
Peggy Fobbe <peggyfobbe@hotmail.com>
2016-11-13 15:54:25 -0500

    Selenium Basics

---
+++ FirstTestCase/NunitTest.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework; 
+
+
+namespace FirstTestCase
+{
+    class NunitTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpenAppTest()
+        {
+            driver.Url = "http://www.demoqa.com";
+            
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
---
+++ FirstTestCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+
+namespace FirstTestCase
+{
+    class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "http://www.demoqa.com";
+
+        }
+    }
+}
