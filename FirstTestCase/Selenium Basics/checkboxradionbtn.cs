eb6cb696ae4aebb535b8995302d2a9e97045f448
Peggy Fobbe <peggyfobbe@hotmail.com>
2016-12-02 20:05:09 -0500

    Selenium Tutorial

--- FirstTestCase/Selenium Basics/checkboxradionbtn.cs
+++ FirstTestCase/Selenium Basics/checkboxradionbtn.cs
@@ -9,19 +9,23 @@ namespace FirstTestCase.Selenium_Basics
 {
     class checkboxradionbtn
     {
+
+        //Launch new browser
+
+        
+        IWebDriver Driver = new ChromeDriver();
+        
         [Test]
 
         public void ChallengeOne()
         {
-            //Launch new browser
-
-            IWebDriver Driver = new ChromeDriver();
+            //Challegene One - Select the deselected Radio Button (female) for category Sex (Use IsSelected Method)
 
             //Open Tools SQA Practice Automation Form
 
             Driver.Url = "http://toolsqa.wpengine.com/automation-practice-form/";
 
-            //Challegene One - Select the deselected Radio Button (female) for category Sex (Use Selected Method)
+            //Challegene One - Select the deselected Radio Button (female) for category Sex (Use IsSelected Method)
 
             IList<IWebElement> RadioBtn = Driver.FindElements(By.Name("sex"));
 
@@ -55,7 +59,9 @@ namespace FirstTestCase.Selenium_Basics
         {
             //Challenge Two - Select the Third radio button for category 'Years of Exp' (Use ID attribute to select radio button
 
-
+            Driver.Url = "http://toolsqa.wpengine.com/automation-practice-form/";
+            IWebElement RadioBtn_Exp = Driver.FindElement(By.Id("exp-2"));
+            RadioBtn_Exp.Click();
 
         }
 
@@ -65,8 +71,32 @@ namespace FirstTestCase.Selenium_Basics
         {
             //Challenge Three – Check the Check Box ‘Automation Tester’ for category ‘Profession'( Use Value attribute to match the selection)
 
+            Driver.Url = "http://toolsqa.wpengine.com/automation-practice-form/";
 
+            //Find the checkbox or radio button element by Name
 
+            IList<IWebElement> chkbox_Profession = Driver.FindElements(By.Name("profession"));
+
+            //This will tell you the number of checkboxes that are present
+
+            int iSize = chkbox_Profession.Count;
+
+            //Start the loop from first checkbox to last checkbox
+            for (int i = 0; i < iSize; i++)
+            {
+                //Store the checkbox name to the string variable, using 'Value' attribute
+
+                String Value = chkbox_Profession.ElementAt(i).GetAttribute("value");
+
+                if (Value.Equals("Automation Tester"))
+                {
+                    //Select the checkbox if the value of the checkbox is the same as what you are looking for
+                    chkbox_Profession.ElementAt(i).Click();
+
+                    //This will take the execution out of loop
+                    break;
+                }
+            }
         }
 
         [Test]
